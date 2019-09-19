4e3d60961b2935abae2767d874931f9e61715afd
Manfred Lange <manfred@urnextgig.com>
2019-09-19 04:20:11 +0000

    Fix for syntax error

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :articles do
     resources :comments
+  end
 
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
